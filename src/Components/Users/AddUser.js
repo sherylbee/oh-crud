ecb1084f2b8c0152bb7bb301adbf491440041d96
sheryl boughton <sherylboughton@gmail.com>
2023-05-27 19:27:32 -0700

    refactor missing value check and set and lift empty warning immediately when all values entered

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -3,30 +3,41 @@ import './AddUser.css'
 
 const AddUser = ({users, onAddUser, onNotifyParent})=>{
     const [user, updateUserAsync] = useState({username:'', age:''});
-    
+    const checkAForMissingValues = (userToCheck)=>{
+        let userValues = [];
+        for(let key in userToCheck){
+            //userValues tracks a list of undefined prop i.e. unfilled form values  
+            if(!user[key]){
+                userValues = [...userValues, key]
+            }
+        }
+        return userValues;
+    }
     const updateUser = (target)=>{
         //this is e.target containing all props of the targeted element
         //see the jsx below for the ~id~ and ~value~ mappings
         const {id, value} = target;
         updateUserAsync({...user, [id]:value})
+        let missingValues = checkAForMissingValues({...user, [id]:value});
+        if(missingValues.length<=1){
+            onNotifyParent("");
+        }
     }
+
     const addUserHandler = (e)=>{
         e.preventDefault();
         user.key = user.dataKey = Math.floor(Math.random()*1000);
         let warningMessage = ""
-        let userValues = [];
-           for(let key in user){
-                if(!user[key]){
-                    userValues = [...userValues, key]
-                }
-            }
-            if(!userValues.length){
+            let missingValues = checkAForMissingValues(user)
+            if(!missingValues.length){
+                //if userValues is empty, everything is defined, so OK to create a user and clear the warning in the parent state for dissemination
                 onAddUser('add', user)
                 updateUserAsync({username:'', age:''})
                 onNotifyParent(warningMessage)
             }
             else{
-                warningMessage = `Please submit ${userValues.join(' and ')}...`
+                //otherwise don't create a user because it has blanks. Instead create a warning message containing the fields to fix and lift it up to the parent
+                warningMessage = `Please submit ${missingValues.join(' and ')}...`
                 onNotifyParent(warningMessage)
             }
     }
